41ede729be55e7409ab8694df71e26a266cf3587
iy2chang <iy2chang@gmail.com>
2015-09-23 14:44:51 -1000

    change the input type for amount and percent to numberDecimal

--- app/src/main/java/com/example/ianc/tipcalculator/MainActivity.java
+++ app/src/main/java/com/example/ianc/tipcalculator/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
